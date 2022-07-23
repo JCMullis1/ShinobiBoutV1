904d6edb52c9a2aa45e15f96ab860d02e7d2ca8a
JCMullis1 <joncalebmullis@gmail.com>
2022-07-22 23:29:06 -0500

    joemama

--- venvdir/main.py
+++ venvdir/main.py
@@ -1,3 +1,20 @@
+from sqlite3 import Timestamp
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.params import Body
 
+app = FastAPI()
 
+class Account(BaseModel):
+    username: str
+    email: str
+    password: str
 
+@app.post("/createaccount")
+def accountreader(payload: dict = Body(...)):
+    print(payload)
+    return {"new_account": f"username {payload['username']} email: {payload['email']} password {payload['password']}"}
+
+@app.get("/Accounts/ID")
+def accountreader():
+    return {"username": username, "email": email, "password": password}
