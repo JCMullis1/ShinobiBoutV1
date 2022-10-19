b0cfcd4da095ab864c496cddb6ebfce4f6d92c7c
Jon Caleb <78839830+JCMullis1@users.noreply.github.com>
2022-10-19 10:15:35 -0500

    this is not empty

--- venvdir/index.js
+++ venvdir/index.js
@@ -2,6 +2,6 @@
 didYou = document.getElementById("didyou")
 
 function pressMeBaby() {
-    didYou.innerText = 'did you really just press me?'
+    didYou.innerText = 'did you really just press me baby?'
     console.log('you pressed me baby')
 }
